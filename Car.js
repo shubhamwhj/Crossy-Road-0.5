54f842dba5f0d33aa9303453340052e131423c5c
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-01-12 16:06:54 +0530

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,18 @@
+class Car{
+   constructor(speed){
+        this.x = random([100,210,320,440,550,660,770,880,990,1100,1210]);
+        this.y = height-random([150,250,350,950,1050,1150,1750,1850,1950]);
+        this.spt = createSprite(this.x,this.y);
+        this.spt.shapeColor = "blue";
+        this.spt.scale=0.5;
+
+        if(this.y==height-250 || this.y==height-1050 || this.y==height-1850)
+        {
+            speed=-speed;
+        }
+        this.spt.velocityX = speed;
+    }
+   
+}
+
+
---
+++ sketch.js
@@ -0,0 +1,30 @@
+var grid = 50;
+var width = 1366;
+var carGroup1,logGroup1;
+var grassHeight = 100;
+var gameState = "play";
+var carAnimation, logAnimation, playerAnimation;
+var school;
+function preload()
+{
+ 
+}
+
+function setup() {
+  createCanvas(1366,2700);
+  carGroup1 = new Group();
+  logGroup1 = new Group();
+  
+  
+ 
+   
+ }
+
+function draw() {
+  background("skyblue");
+ 
+ 
+
+  drawSprites();
+}
+
